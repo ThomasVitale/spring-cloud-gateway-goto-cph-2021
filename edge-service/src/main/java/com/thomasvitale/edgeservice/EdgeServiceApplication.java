3fa172035b0acab965eacb7f5e720a351482404d
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-11-07 23:39:29 +0100

    Security

--- edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
@@ -1,15 +1,19 @@
 package com.thomasvitale.edgeservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@EnableWebFluxSecurity
 public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
@@ -17,8 +21,12 @@ public class EdgeServiceApplication {
 	}
 
 	@Bean
-	KeyResolver keyResolver() {
-		return exchange -> Mono.just("ANONYMOUS");
+	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		return http
+				.authorizeExchange(exchange -> exchange.matchers(EndpointRequest.toAnyEndpoint()).permitAll()
+						.anyExchange().authenticated())
+				.oauth2Login(Customizer.withDefaults())
+				.build();
 	}
 
 }
