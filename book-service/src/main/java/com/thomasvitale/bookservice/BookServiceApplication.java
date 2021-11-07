efe6919c8a617fa65351414b66527eb11efcb0e4
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-11-07 22:58:42 +0100

    Retries

--- book-service/src/main/java/com/thomasvitale/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/com/thomasvitale/bookservice/BookServiceApplication.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -32,6 +34,12 @@ class BookController {
                 new Book("The Hobbit"),
                 new Book("The Lord of the Rings")
         ).doFirst(() -> log.info("Returning list of books in the catalog"));
+    }
+
+    @GetMapping("books/fail")
+    public ResponseEntity<Book> serviceUnavailable() {
+        log.info("Attempting to return the books in the catalog");
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
     }
 
 }
